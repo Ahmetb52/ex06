f9bd127e06b4a580203aa5837a1b8809b4ffcbd9
Ahmetb52 <82288871+Ahmetb52@users.noreply.github.com>
2023-09-04 03:26:28 +0300

    Add files via upload

---
+++ ex06/ft_print_comb2.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sekmekci <sekmekci@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/31 17:06:53 by sekmekci          #+#    #+#             */
+/*   Updated: 2023/09/03 13:21:24 by sekmekci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write (1, &c, 1);
+}
+
+void	print_int(int i)
+{
+	ft_putchar(48 + i);
+}
+
+void	ft_print(int a, int b)
+{
+	print_int(a / 10);
+	print_int(a % 10);
+	ft_putchar(' ');
+	print_int(b / 10);
+	print_int(b % 10);
+	if (a != 98)
+		write(1, ", ", 2);
+}
+
+void	ft_print_comb2(void)
+{
+	int	a;
+	int	b;
+
+	a = 0;
+	b = 1;
+	while (a < 99)
+	{
+		while (b < 100)
+		{
+			ft_print(a, b);
+			b++;
+		}
+		a++;
+		b = a + 1;
+	}
+}
+
+int	main(void)
+{
+	ft_print_comb2();
+}
